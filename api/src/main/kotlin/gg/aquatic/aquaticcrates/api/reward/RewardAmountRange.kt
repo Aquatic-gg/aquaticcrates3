6569e25d3822ffb5ff4651a762b29cfff4ea9235
MrLarkyy_ <studiotadikl@gmail.com>
2024-10-25 00:03:22 +0200

    Optimize random number generation in RewardAmountRange
    Handle cases where min equals max directly, avoiding unnecessary function calls. Also, ensure the random range is correctly computed using min and max functions to guard against potential inversions.

--- api/src/main/kotlin/gg/aquatic/aquaticcrates/api/reward/RewardAmountRange.kt
+++ api/src/main/kotlin/gg/aquatic/aquaticcrates/api/reward/RewardAmountRange.kt
@@ -1,6 +1,8 @@
 package gg.aquatic.aquaticcrates.api.reward
 
 import gg.aquatic.aquaticseries.lib.chance.IChance
+import kotlin.math.max
+import kotlin.math.min
 import kotlin.random.Random
 
 class RewardAmountRange(
@@ -11,7 +13,10 @@ class RewardAmountRange(
 
     val randomNum: Int
         get() {
-            return Random.nextInt(min, max + 1)
+            if (min == max) {
+                return min
+            }
+            return Random.nextInt(min(min, max), max(min, max) + 1)
         }
 
 }
