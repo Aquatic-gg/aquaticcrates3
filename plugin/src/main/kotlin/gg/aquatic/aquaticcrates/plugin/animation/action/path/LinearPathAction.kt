4ac81dc99addf9910134ecfd1322678eddc4ffc9
MrLarkyy_ <studiotadikl@gmail.com>
2024-10-26 16:00:06 +0200

    Add support for LinearPathProp in LinearPathAction
    Refactor the run method to initialize and bind a LinearPathProp object using the provided id and points. Include a new PathPointsArgument in the arguments list to support this change.

--- plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/animation/action/path/LinearPathAction.kt
+++ plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/animation/action/path/LinearPathAction.kt
@@ -1,20 +1,29 @@
 package gg.aquatic.aquaticcrates.plugin.animation.action.path
 
 import gg.aquatic.aquaticcrates.api.animation.Animation
+import gg.aquatic.aquaticcrates.plugin.animation.prop.path.LinearPathProp
+import gg.aquatic.aquaticcrates.plugin.animation.prop.path.PathPoint
 import gg.aquatic.aquaticseries.lib.action.AbstractAction
 import gg.aquatic.aquaticseries.lib.util.argument.AquaticObjectArgument
 import gg.aquatic.aquaticseries.lib.util.argument.impl.PrimitiveObjectArgument
+import java.util.*
 import java.util.function.BiFunction
 
 class LinearPathAction: AbstractAction<Animation>() {
     override fun run(binder: Animation, args: Map<String, Any?>, textUpdater: BiFunction<Animation, String, String>) {
+        val id = args["id"] as String
+        val points = args["points"] as TreeMap<Int, PathPoint>
 
+        val path = LinearPathProp(
+            points, binder
+        )
+        binder.props["path:$id"] = path
     }
 
     override fun arguments(): List<AquaticObjectArgument<*>> {
         return listOf(
             PrimitiveObjectArgument("id", "linear-path1", true),
-
+            PathPointsArgument("points", TreeMap<Int, PathPoint>(), true),
         )
     }
 }
