623df7db0486ac9b2878ca160519071e18720198
MrLarkyy_ <studiotadikl@gmail.com>
2025-01-27 19:57:22 +0100

    Refactor ConditionalActionsArgument and add PlayerBound support
    Refactored ConditionalActionsArgument for better structure and readability by introducing ConditionalAnimationActions. Added support for PlayerBoundAnimation actions, conditions, and fail actions to enable more versatile functionality.

--- api/src/main/kotlin/gg/aquatic/aquaticcrates/api/util/ConditionalActionsArgument.kt
+++ api/src/main/kotlin/gg/aquatic/aquaticcrates/api/util/ConditionalActionsArgument.kt
@@ -1,76 +1,91 @@
 package gg.aquatic.aquaticcrates.api.util
 
 import gg.aquatic.aquaticcrates.api.animation.Animation
+import gg.aquatic.aquaticcrates.api.animation.PlayerBoundAnimation
 import gg.aquatic.waves.registry.serializer.ActionSerializer
 import gg.aquatic.waves.registry.serializer.RequirementSerializer
 import gg.aquatic.waves.util.argument.AbstractObjectArgumentSerializer
 import gg.aquatic.waves.util.argument.AquaticObjectArgument
+import gg.aquatic.waves.util.checkRequirements
+import gg.aquatic.waves.util.executeActions
+import gg.aquatic.waves.util.generic.ConfiguredExecutableObject
 import gg.aquatic.waves.util.generic.ConfiguredExecutableObjectWithConditions
 import gg.aquatic.waves.util.generic.ConfiguredExecutableObjectsWithConditions
 import gg.aquatic.waves.util.getSectionList
+import gg.aquatic.waves.util.requirement.ConfiguredRequirement
 import gg.aquatic.waves.util.requirement.ConfiguredRequirementWithFailActions
 import org.bukkit.configuration.ConfigurationSection
 import java.util.ArrayList
 
-class ConditionalActionsArgument(id: String, defaultValue: ConfiguredExecutableObjectsWithConditions<Animation, Unit>?,
-                                 required: Boolean
-) : AquaticObjectArgument<ConfiguredExecutableObjectsWithConditions<Animation,Unit>>(
+class ConditionalActionsArgument(
+    id: String, defaultValue: ConditionalAnimationActions?,
+    required: Boolean
+) : AquaticObjectArgument<ConditionalActionsArgument.ConditionalAnimationActions>(
     id, defaultValue, required
 ) {
-    override val serializer: AbstractObjectArgumentSerializer<ConfiguredExecutableObjectsWithConditions<Animation, Unit>?>
+    override val serializer: AbstractObjectArgumentSerializer<ConditionalAnimationActions?>
         get() = Serializer
 
-    override fun load(section: ConfigurationSection): ConfiguredExecutableObjectsWithConditions<Animation, Unit>? {
+    override fun load(section: ConfigurationSection): ConditionalAnimationActions {
         return Serializer.load(section, "")
     }
 
-    object Serializer : AbstractObjectArgumentSerializer<ConfiguredExecutableObjectsWithConditions<Animation, Unit>?>() {
+    object Serializer :
+        AbstractObjectArgumentSerializer<ConditionalAnimationActions?>() {
         override fun load(
             section: ConfigurationSection,
             id: String
-        ): ConfiguredExecutableObjectsWithConditions<Animation, Unit>? {
-            return loadActionsWithConditions(section)
+        ): ConditionalAnimationActions {
+            val actions = ActionSerializer.fromSections<Animation>(section.getSectionList("actions"))
+            val playerBoundActions =
+                ActionSerializer.fromSections<PlayerBoundAnimation>(section.getSectionList("actions"))
+
+            val conditions = RequirementSerializer.fromSections<Animation>(section.getSectionList("conditions"))
+            val playerBoundConditions =
+                RequirementSerializer.fromSections<PlayerBoundAnimation>(section.getSectionList("conditions"))
+
+            val failActions = ActionSerializer.fromSections<Animation>(section.getSectionList("fail"))
+            val failPlayerBoundActions =
+                ActionSerializer.fromSections<PlayerBoundAnimation>(section.getSectionList("fail"))
+
+            return ConditionalAnimationActions(
+                actions,
+                playerBoundActions,
+                conditions,
+                playerBoundConditions,
+                failActions,
+                failPlayerBoundActions
+            )
         }
+    }
 
-        fun loadActionsWithConditions(section: ConfigurationSection): ConfiguredExecutableObjectsWithConditions<Animation,Unit>? {
-            val actions = ArrayList<ConfiguredExecutableObjectWithConditions<Animation,Unit>>()
-            val actionSections = section.getSectionList("actions")
+    class ConditionalAnimationActions(
+        val actions: List<ConfiguredExecutableObject<Animation, Unit>>,
+        val playerBoundActions: List<ConfiguredExecutableObject<PlayerBoundAnimation, Unit>>,
+        val conditions: List<ConfiguredRequirement<Animation>>,
+        val playerBoundConditions: List<ConfiguredRequirement<PlayerBoundAnimation>>,
+        val failActions: List<ConfiguredExecutableObject<Animation, Unit>>,
+        val failPlayerBoundActions: List<ConfiguredExecutableObject<PlayerBoundAnimation, Unit>>
+    ) {
 
-            for (actionSection in actionSections) {
-                actions += loadActionWithCondition(actionSection) ?: continue
+        fun tryExecute(animation: Animation) {
+            var isMet = true
+            if (animation is PlayerBoundAnimation) {
+                if (!playerBoundConditions.checkRequirements(animation)) isMet = false
             }
-            val conditions = ArrayList<ConfiguredRequirementWithFailActions<Animation,Unit>>()
-            for (conditionSection in section.getSectionList("conditions")) {
-                conditions += loadConditionWithFailActions(conditionSection) ?: continue
+            if (isMet && !conditions.checkRequirements(animation)) isMet = false
+
+            if (isMet) {
+                actions.forEach { it.execute(animation) { _, str -> animation.updatePlaceholders(str) } }
+                if (animation is PlayerBoundAnimation) {
+                    playerBoundActions.forEach { it.execute(animation) { _, str -> animation.updatePlaceholders(str) } }
+                }
+            } else {
+                failActions.executeActions(animation) { _, str -> animation.updatePlaceholders(str) }
+                if (animation is PlayerBoundAnimation) {
+                    failPlayerBoundActions.executeActions(animation) { _, str -> animation.updatePlaceholders(str) }
+                }
             }
-
-            if (actions.isEmpty() && conditions.isEmpty()) return null
-
-            val failActions = if (section.isConfigurationSection("fail") && conditions.isNotEmpty()) {
-                loadActionsWithConditions(section.getConfigurationSection("fail")!!)
-            } else null
-
-            return ConfiguredExecutableObjectsWithConditions(actions, conditions, failActions)
-        }
-
-        fun loadActionWithCondition(section: ConfigurationSection): ConfiguredExecutableObjectWithConditions<Animation, Unit>? {
-            val action = ActionSerializer.fromSection<Animation>(section) ?: return null
-            val conditions = ArrayList<ConfiguredRequirementWithFailActions<Animation,Unit>>()
-            for (configurationSection in section.getSectionList("conditions")) {
-                conditions += loadConditionWithFailActions(configurationSection) ?: continue
-            }
-            val failActions = if (section.isConfigurationSection("fail") && conditions.isNotEmpty()) {
-                loadActionsWithConditions(section.getConfigurationSection("fail")!!)
-            } else null
-            return ConfiguredExecutableObjectWithConditions(action, conditions, failActions)
-        }
-
-        fun loadConditionWithFailActions(section: ConfigurationSection): ConfiguredRequirementWithFailActions<Animation, Unit>? {
-            val condition = RequirementSerializer.fromSection<Animation>(section) ?: return null
-            val failActions = if (section.isConfigurationSection("fail")) {
-                loadActionsWithConditions(section.getConfigurationSection("fail")!!)
-            } else null
-            return ConfiguredRequirementWithFailActions(condition, failActions)
         }
 
     }
