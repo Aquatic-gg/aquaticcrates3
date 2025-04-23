26025ddc2f7c6909c4d9e3166222acb9f7d9cf36
MrLarkyy_ <studiotadikl@gmail.com>
2025-04-23 14:24:30 +0200

    Update PotionEffectsAction to use NamespacedKey for potions
    Switched to using `NamespacedKey` with `Registry.POTION_EFFECT_TYPE` for fetching potion effect types. This improves compatibility with Minecraft's namespace-based resource system.

--- plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/animation/action/potion/PotionEffectsAction.kt
+++ plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/animation/action/potion/PotionEffectsAction.kt
@@ -6,6 +6,8 @@ import gg.aquatic.waves.util.argument.AquaticObjectArgument
 import gg.aquatic.waves.util.argument.ObjectArguments
 import gg.aquatic.waves.util.generic.Action
 import gg.aquatic.waves.util.getSectionList
+import org.bukkit.NamespacedKey
+import org.bukkit.Registry
 import org.bukkit.configuration.ConfigurationSection
 import org.bukkit.potion.PotionEffect
 import org.bukkit.potion.PotionEffectType
@@ -42,8 +44,8 @@ class PotionEffectsAction : Action<PlayerBoundAnimation> {
             override fun load(section: ConfigurationSection, id: String): Map<PotionEffectType, Pair<Int, Int>>? {
                 val map = mutableMapOf<PotionEffectType, Pair<Int, Int>>()
                 for (configurationSection in section.getSectionList(id)) {
-                    val type =
-                        PotionEffectType.getByName(configurationSection.getString("potion") ?: continue) ?: continue
+
+                    val type = Registry.POTION_EFFECT_TYPE.get(NamespacedKey.minecraft(configurationSection.getString("potion") ?: continue)) ?: continue
                     val duration = configurationSection.getInt("duration")
                     val amplifier = configurationSection.getInt("amplifier")
                     map += type to (duration to amplifier)
