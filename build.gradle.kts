39d9f3e3900ff6d18b851976b58a776b6516c6a7
MrLarkyy_ <studiotadikl@gmail.com>
2025-06-10 16:53:45 +0200

    Refactor entity data handling and update Waves dependency
    Streamlined entity data creation using new `ConfiguredEntityData` structure for improved readability and maintainability. Updated Waves dependency from version 1.2.12 to 1.2.17. Adjusted entity property logic to align with updated API changes.

--- build.gradle.kts
+++ build.gradle.kts
@@ -52,7 +52,7 @@ subprojects {
         compileOnly("com.github.LoneDev6:API-ItemsAdder:3.6.2-beta-r3-b")
         compileOnly("gg.aquatic.comet:Comet-API:1.4.0")
         compileOnly ("com.ticxo.modelengine:ModelEngine:R4.0.8")
-        compileOnly("gg.aquatic.waves:Waves:1.2.17:publish")
+        compileOnly("gg.aquatic.waves:Waves:1.2.18:publish")
         //implementation("net.kyori:adventure-api:4.17.0")
     }
 
--- plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/animation/prop/entity/property/impl/EntityDataProperty.kt
+++ plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/animation/prop/entity/property/impl/EntityDataProperty.kt
@@ -5,6 +5,7 @@ import gg.aquatic.aquaticcrates.plugin.animation.prop.entity.property.EntityProp
 import gg.aquatic.aquaticcrates.plugin.animation.prop.entity.property.EntityPropertySerializer
 import gg.aquatic.waves.fake.entity.FakeEntity
 import gg.aquatic.waves.fake.entity.data.ConfiguredEntityData
+import gg.aquatic.waves.fake.entity.data.EntityData
 import gg.aquatic.waves.registry.WavesRegistry
 import gg.aquatic.waves.util.argument.ArgumentSerializer
 import gg.aquatic.waves.util.argument.ObjectArguments
@@ -23,7 +24,11 @@ class EntityDataProperty(
     object Serializer : EntityPropertySerializer {
         override fun load(section: ConfigurationSection): EntityProperty {
             return EntityDataProperty(section.getKeys(false).mapNotNull { id ->
-                val type = WavesRegistry.ENTITY_DATA[id] ?: return@mapNotNull null
+                val types = HashMap<String, EntityData>()
+                for (map in WavesRegistry.ENTITY_DATA.values) {
+                    types += map
+                }
+                val type = types[id] ?: return@mapNotNull null
                 val arguments = ObjectArguments(ArgumentSerializer.load(section,type.arguments))
                 ConfiguredEntityData(type,arguments)
             })
