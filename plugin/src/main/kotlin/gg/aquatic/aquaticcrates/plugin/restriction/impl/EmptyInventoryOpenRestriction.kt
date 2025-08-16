57df9759d187867dc0726e6fc8e72d253f3d36ad
MrLarkyy_ <studiotadikl@gmail.com>
2025-08-16 20:08:51 +0200

    Add slot-based inventory check to EmptyInventoryOpenRestriction
    Enhanced `EmptyInventoryOpenRestriction` to include a configurable "slots" argument, allowing checks based on the number of empty slots in the player's inventory. Updated related logic to handle the new argument gracefully.

--- plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/restriction/impl/EmptyInventoryOpenRestriction.kt
+++ plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/restriction/impl/EmptyInventoryOpenRestriction.kt
@@ -4,15 +4,21 @@ import gg.aquatic.aquaticcrates.plugin.restriction.OpenData
 import gg.aquatic.aquaticcrates.plugin.restriction.OpenRestriction
 import gg.aquatic.waves.util.argument.AquaticObjectArgument
 import gg.aquatic.waves.util.argument.ObjectArguments
+import gg.aquatic.waves.util.argument.impl.PrimitiveObjectArgument
 import org.bukkit.Material
 
 class EmptyInventoryOpenRestriction: OpenRestriction() {
-    override val arguments: List<AquaticObjectArgument<*>> = listOf()
+    override val arguments: List<AquaticObjectArgument<*>> = listOf(
+        PrimitiveObjectArgument("slots",1, false)
+    )
 
     override fun execute(binder: OpenData, args: ObjectArguments, textUpdater: (OpenData, String) -> String): Boolean {
+        val slots = args.int("slots") { textUpdater(binder, it) } ?: return true
         val player = binder.player
+        var count = 0
         for (storageContent in player.inventory.storageContents) {
-            if (storageContent == null || storageContent.type == Material.AIR) return true
+            if (count >= slots) return true
+            if (storageContent == null || storageContent.type == Material.AIR) count++
         }
         return false
     }
