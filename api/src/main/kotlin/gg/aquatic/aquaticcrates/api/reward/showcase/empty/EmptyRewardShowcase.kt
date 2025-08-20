71daaee3070882b1d8663ebf8831f65efcae31fd
MrLarkyy_ <studiotadikl@gmail.com>
2025-08-21 01:18:47 +0200

    Fix `EmptyRewardShowcaseHandle` return in `EmptyRewardShowcase` creation

--- api/src/main/kotlin/gg/aquatic/aquaticcrates/api/reward/showcase/empty/EmptyRewardShowcase.kt
+++ api/src/main/kotlin/gg/aquatic/aquaticcrates/api/reward/showcase/empty/EmptyRewardShowcase.kt
@@ -5,11 +5,11 @@ import gg.aquatic.aquaticcrates.api.animation.PlayerBoundAnimation
 import gg.aquatic.aquaticcrates.api.reward.Reward
 import gg.aquatic.aquaticcrates.api.reward.showcase.RewardShowcase
 import gg.aquatic.aquaticcrates.api.reward.showcase.RewardShowcaseHandle
-import gg.aquatic.aquaticcrates.api.reward.showcase.item.ItemRewardShowcaseHandle
 import gg.aquatic.waves.hologram.LineSettings
 import gg.aquatic.waves.interactable.settings.InteractableSettings
 import gg.aquatic.waves.util.generic.ConfiguredExecutableObject
 import org.bukkit.util.Vector
+import org.joml.Vector3d
 
 class EmptyRewardShowcase(
     override val hologram: Collection<LineSettings>,
@@ -24,7 +24,7 @@ class EmptyRewardShowcase(
         reward: Reward,
         locationOffset: Pair<Vector, Pair<Float, Float>>
     ): RewardShowcaseHandle<*> {
-        return ItemRewardShowcaseHandle(animation, this, locationOffset, reward)
+        return EmptyRewardShowcaseHandle(animation, this, locationOffset, reward)
     }
 
 
