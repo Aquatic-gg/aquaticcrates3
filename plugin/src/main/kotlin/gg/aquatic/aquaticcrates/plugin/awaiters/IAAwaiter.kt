8d0c53fa6022b874bb9106ba9fdd28d28935cff4
MrLarkyy_ <studiotadikl@gmail.com>
2025-10-17 10:50:02 +0200

    Add ItemsAdder compatibility: - Verify item load status before attaching event listener. - Update plugin YAML to include ItemsAdder as an optional dependency.

--- plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/awaiters/IAAwaiter.kt
+++ plugin/src/main/kotlin/gg/aquatic/aquaticcrates/plugin/awaiters/IAAwaiter.kt
@@ -1,6 +1,7 @@
 package gg.aquatic.aquaticcrates.plugin.awaiters
 
 import dev.lone.itemsadder.api.Events.ItemsAdderLoadDataEvent
+import dev.lone.itemsadder.api.ItemsAdder
 import gg.aquatic.waves.api.event.event
 import java.util.concurrent.CompletableFuture
 
@@ -8,9 +9,14 @@ class IAAwaiter: AbstractAwaiter() {
     override val future: CompletableFuture<Void> = CompletableFuture()
 
     init {
-        event<ItemsAdderLoadDataEvent> {
+        if (ItemsAdder.areItemsLoaded()) {
             future.complete(null)
             loaded = true
+        } else {
+            event<ItemsAdderLoadDataEvent> {
+                future.complete(null)
+                loaded = true
+            }
         }
     }
 }
